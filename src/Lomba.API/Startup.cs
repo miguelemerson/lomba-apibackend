b0180be7c839d39cdb2a71fc53ce685932b2780c
miguelemerson <mperedo@gmail.com>
2022-06-07 17:47:15 -0400

    Se agrega posibilidad de autenticar en Swagger
    Es posible especificar token en swagger

--- src/Lomba.API/Startup.cs
+++ src/Lomba.API/Startup.cs
@@ -3,6 +3,8 @@ using Lomba.API.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
 
 namespace Lomba.API
 {
@@ -41,7 +43,42 @@ namespace Lomba.API
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Lomba API",
+                    Version = "v1",
+                    Description = "Backend Lomba API"
+                });
+
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "JWT Authorization header using the Bearer scheme."
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                    {
+                        {
+                              new OpenApiSecurityScheme
+                                {
+                                    Reference = new OpenApiReference
+                                    {
+                                        Type = ReferenceType.SecurityScheme,
+                                        Id = "Bearer"
+                                    }
+                                },
+                                new string[] {}
+
+                        }
+                    });
+            });
+
             builder.Services.AddDbContext<DataContext>();
             builder.Services.AddScoped<IUserService, UserService>();
             builder.Services.AddScoped<IOrgaService, OrgaService>();
