123b9b242686626be81a9736be292a49bb304e90
miguelemerson <mperedo@gmail.com>
2022-07-04 19:28:41 -0400

    Se quita Console Write con string de base de datos.

--- src/Lomba.API/Contexts/DataContext.cs
+++ src/Lomba.API/Contexts/DataContext.cs
@@ -13,8 +13,6 @@ namespace Lomba.API.Contexts
 
         protected override void OnConfiguring(DbContextOptionsBuilder options)
         {
-            Console.WriteLine(Configuration.GetConnectionString("SQLServerDatabase"));
-
             options.UseSqlServer(connectionString: Configuration.GetConnectionString("SQLServerDatabase"),
                 options => options.EnableRetryOnFailure());
         }
